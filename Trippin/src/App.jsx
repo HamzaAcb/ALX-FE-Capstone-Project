6190e9b728f1f633957466d96da0b258cb618f10
HamzaAcb <hamzaachoubir98@gmail.com>
2024-09-27 14:17:05 +0300

    Adding the header,hero and top destinations components to the main app

---
+++ Trippin/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import Hero from "./components/custom/Hero";
+import Header from "./components/custom/Header";
+import Destination from "./components/custom/Destination";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="h-screen bg-cover bg-center bg-no-repeat bg-[url('/hero.jpg')]">
+      <Header />
+      <Hero />
+      <Destination />
+    </div>
+  );
+}
+
+export default App;
