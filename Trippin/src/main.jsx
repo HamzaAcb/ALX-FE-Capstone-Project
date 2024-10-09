b922d7e6fb499d1f338881bb9f00730c2ecde63c
HamzaAcb <hamzaachoubir98@gmail.com>
2024-10-09 13:44:54 +0300

    Including new components

--- Trippin/src/main.jsx
+++ Trippin/src/main.jsx
@@ -2,10 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import Header from "./components/custom/Header.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Activities from "./pages/Activities.jsx";
+import Flights from "./pages/Flights.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Flights",
+    element: <Flights />,
+  },
+  {
+    path: "/Activities",
+    element: <Activities />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
+  // Using createRoot directly
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
