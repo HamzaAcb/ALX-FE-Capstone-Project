dcbf1b51c0b403ed475fe2aae881198d79b74aa1
HamzaAcb <hamzaachoubir98@gmail.com>
2024-10-10 19:20:59 +0300

    Modifying header components position

--- Trippin/src/components/custom/Header.jsx
+++ Trippin/src/components/custom/Header.jsx
@@ -4,12 +4,16 @@ import { Link } from "react-router-dom"; // Import Link for navigation
 /* creating the header section */
 const Header = () => {
   return (
-    <div className="p-4 flex flex-col md:flex-row justify-between items-center">
-      {/* Logo */}
-      <img src="/Logo.svg" alt="Logo" className="h-12 w-24 mb-4 md:mb-0" />
+    <div className="p-4 flex flex-col md:flex-row items-center">
+      {/* Logo aligned to the left */}
+      <img
+        src="/Logo.svg"
+        alt="Logo"
+        className="h-12 w-24 md:mr-auto mb-4 md:mb-0"
+      />
 
       {/* Centering the links for larger screens, stacking for smaller screens */}
-      <div className="font-bold text-purple-900 flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-8">
+      <div className="font-bold text-purple-900 flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-8 md:flex-grow justify-center">
         <Link to="/" className="hover:text-purple-500 underline">
           Home
         </Link>
